877977bfb8829d0ab24b29031a348cf76ddb8d64
Abdallah <abdallahabohamad12@gmail.com>
2024-09-06 07:37:20 +0300

    Finished Sebha Fragment

--- app/src/main/java/com/androidapps/islamiapp/fragments/sebha/SebhaFragment.kt
+++ app/src/main/java/com/androidapps/islamiapp/fragments/sebha/SebhaFragment.kt
@@ -6,13 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androidapps.islamiapp.R
+import com.androidapps.islamiapp.databinding.FragmentSebhaBinding
 
 class SebhaFragment : Fragment() {
-
+    private lateinit var binding: FragmentSebhaBinding
+    private var counter = 0
+    private var currentIndex = 0
+    private lateinit var azkarList: MutableList<String>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sebha, container, false)
+    ): View {
+        binding = FragmentSebhaBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.azkarList).toMutableList()
+        binding.idTsabehatText.text = azkarList[0]
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        binding.idBodyOfSebhaImage.setOnClickListener {
+            binding.idBodyOfSebhaImage.rotation += (360 / 33).toFloat()
+            if (counter < 33) {
+                counter++
+            } else {
+                counter = 0
+                currentIndex = if (currentIndex < azkarList.size - 1) {
+                    ++currentIndex
+                } else {
+                    0
+                }
+                binding.idTsabehatText.text = azkarList[currentIndex]
+            }
+            binding.idNumberOfTsabehatNumber.text = counter.toString()
+        }
+    }
+
 }
