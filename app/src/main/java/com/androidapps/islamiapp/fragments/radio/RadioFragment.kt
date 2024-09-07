77522cacce5cab4824f1a1c141aab11e3ee40a16
Abdallah <abdallahabohamad12@gmail.com>
2024-09-07 05:21:38 +0300

    finished Radio Fragment ui

--- app/src/main/java/com/androidapps/islamiapp/fragments/radio/RadioFragment.kt
+++ app/src/main/java/com/androidapps/islamiapp/fragments/radio/RadioFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.androidapps.islamiapp.R
+import com.androidapps.islamiapp.databinding.FragmentRadioBinding
 
 class RadioFragment : Fragment() {
-
+    private lateinit var binding: FragmentRadioBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_radio, container, false)
+    ): View {
+        binding = FragmentRadioBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
